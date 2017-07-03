e8cd456e3b05dea1f0167afaa66c0a65e554c810
liuman2 <liuman2@163.com>
2017-07-04 01:32:38 +0800

    update

--- src/view/account/view/sub.js
+++ src/view/account/view/sub.js
@@ -4,6 +4,8 @@ module.exports = function($scope, $state, $http, $cookieStore, $timeout) {
     sub_id = $state.params.sub_id || null,
     dInput = $('.date-input');
 
+  var user = $cookieStore.get('USER_INFO');
+
   var jForm = $('#accounting_sub_modal');
   jForm.validator({
     theme: 'yellow_right',
@@ -42,11 +44,17 @@ module.exports = function($scope, $state, $http, $cookieStore, $timeout) {
       break;
   }
 
+  $scope.activeTab = 0;
+  $scope.onTab = function(activeIndex) {
+    $scope.activeTab = activeIndex;
+  }
+
   var user = $cookieStore.get('USER_INFO');
   $scope.subData = {
     id: '',
     date_start: '',
     date_end: '',
+    salesman_id: user.id,
   }
 
   if (sub_id) {
@@ -70,14 +78,57 @@ module.exports = function($scope, $state, $http, $cookieStore, $timeout) {
     }
   }
 
+  function valid_salesman() {
+    if (!$scope.subData.salesman_id) {
+      jForm.validator('showMsg', '#salesmanSelect2-validator', {
+        type: "error",
+        msg: "此处不能为空"
+      });
+      return false;
+    } else {
+      jForm.validator('hideMsg', '#salesmanSelect2-validator');
+      return true;
+    }
+  }
+
   $scope.save = function() {
     jForm.isValid(function(v) {
+      if (!v) {
+        if (
+          !$('select[name="tax"]').isValid() ||
+          !$('input[name="date_start"]').isValid() ||
+          !$('input[name="date_end"]').isValid() ||
+          !$('input[name="amount_transaction"]').isValid()) {
+
+          $scope.activeTab = 0;
+          $scope.$apply();
+          return;
+        }
+        if (
+          !$('input[name="invoice_bank"]').isValid() ||
+          !$('input[name="invoice_account"]').isValid() ||
+          !$('input[name="invoice_name"]').isValid() ||
+          !$('input[name="invoice_tax"]').isValid() ||
+          !$('input[name="invoice_address"]').isValid()) {
+
+          $scope.activeTab = 1;
+          $scope.$apply();
+          return;
+        }
+      }
+
+      var isSalesmanVaild = valid_salesman();
+      if (!isSalesmanVaild) {
+        $scope.activeTab = 2;
+        $scope.$apply();
+        return;
+      }
+
       if (v) {
         var submitData = angular.copy($scope.subData);
         submitData.date_start = $('#date_start').val();
         submitData.date_end = $('#date_end').val();
         submitData.master_id = master_id;
-
         var url = $scope.action == 'add' ? '/Accounting/AddItem' : '/Accounting/UpdateItem';
         $http({
           method: 'POST',
